5e6851223c49489f6feea3295f0cbdbcbbad222f
Zac Emerzian <zemerzia@ucsc.edu>
2025-04-16 13:49:59 -0700

    Fix Gibdo sprite filter

--- Day 8 Demo/main.lua
+++ Day 8 Demo/main.lua
@@ -3,9 +3,6 @@
 -- 4-7-2025 - 4-16-2025
 io.stdout:setvbuf("no")
 
-require "entity"
-require "spriteClass"
-
 function love.load()
   love.window.setTitle("Zelda Demo")
   love.graphics.setDefaultFilter("nearest", "nearest")
@@ -13,6 +10,9 @@ function love.load()
   screenHeight = 480
   love.window.setMode(screenWidth, screenHeight)
   love.graphics.setBackgroundColor(0.2, 0.7, 0.2, 1)
+  
+  require "entity"
+  require "spriteClass" -- require will call the functions in the given file, so the Gibdo sprite WAS being loaded in BEFORE we were setting the default filter in load(), by moving the requires into load(), we can setup the project before getting the other scripts involved
   
   linkSpriteClass = LinkSprites:new()
   moblinSpriteClass = MoblinSprites:new()
@@ -58,7 +58,6 @@ function love.load()
       )
     end
   end
-  
 end
 
 function love.update()
