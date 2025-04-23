63fa5da41d60a8c3ed1ad7826a36a11200b933e3
Zac Emerzian <zemerzia@ucsc.edu>
2025-04-23 13:09:52 -0700

    Day 10 Demo - refactoring code samples

--- Day 10 Demo/sample3.lua
+++ Day 10 Demo/sample3.lua
@@ -1,9 +1,9 @@
 
 -- Original Code Sample #3
 -- in the draw loop for a card, with the heading
-
+-- Similar ideas to sample1
 -- set it to red or black
-if self.suit == "Hearts" or self.suit == "Diamonds" then
+if self.suit == "Hearts" or self.suit == "Diamnds" then
   love.graphics.setColor(1, 0, 0)
 else
   love.graphics.setColor(0, 0, 0)
--- Day 10 Demo/sample4.lua
+++ Day 10 Demo/sample4.lua
@@ -2,13 +2,20 @@
 -- Original Code Sample #4
 -- In love.load()
 
+SUIT_ENUM = {
+  SPADE,
+  CLUB, 
+  HEART,
+  DIAMOND
+}
+
 cards = {}
 
-local suits = {"S", "H", "D", "C"}
+local suitPrefix = {"S", "C", "H", "D"}
 local ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
-for i=1,4 do
-    for j=1,13 do
-        table.insert(cards, love.graphics.newImage("sprites/" .. suits[i] .. ranks[j] .. ".png"))
+for suit in SUIT_ENUM do
+    for j = 1, #ranks do
+        table.insert(cards, love.graphics.newImage("sprites/" .. suitPrefix[suit] .. ranks[j] .. ".png"))
     end
 end
